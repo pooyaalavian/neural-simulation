e1c53c65b414b0b06d8965921c4d574a56fbf732
Pooya Alavian <alavian@smartproductionsystems.com>
2023-03-06 14:29:04 -0500

    fix initial values

--- src/model_base.py
+++ src/model_base.py
@@ -3,20 +3,25 @@ from src.state import State, NodeState
 from src.param import ParameterSet
 from src.nodes import NodeParam
 from src.constants import ConstantsParam
+import logging
+
+
+def arr2str(arr: np.array):
+    return ', '.join([f'{x:12.3f}' for x in arr])
 
 
 class ModelBase(State):
 
     def initialize(self):
-        self.exc1.s = 0.5
-        self.exc2.s = 0.5
-        self.pv.s = 0.3
-        self.exc1.s_ampa = 0.5
-        self.exc2.s_ampa = 0.5
-        self.sst1.s = 0.5
-        self.sst2.s = 0.5
-        self.vip1.s = 0.5
-        self.vip2.s = 0.5
+        self.exc1.s = 0.05
+        self.exc2.s = 0.05
+        self.pv.s = 0.03
+        self.exc1.s_ampa = 0.05
+        self.exc2.s_ampa = 0.05
+        self.sst1.s = 0.05
+        self.sst2.s = 0.05
+        self.vip1.s = 0.05
+        self.vip2.s = 0.05
 
     def calc_inputs(self, t: np.float64, p: ParameterSet):
         for dest in self.Nodes:
@@ -31,30 +36,40 @@ class ModelBase(State):
             # for each node in graph, clacl flow to this node
             for src in self.Nodes:
                 n_src: NodeState = getattr(self, src)
-                p_src: NodeParam = getattr(p, src)
-                # TODO: do we need to skip src==dest?
-                # if src == dest:
-                #     continue
-                n_dest._input += n_src.s * \
-                    p.J.get(src, dest) + n_src.s_ampa * p.J_ampa.get(src, dest)
+                n_dest._input += n_src.s * p.J.get(src, dest)
+                n_dest._input += n_src.s_ampa * p.J_ampa.get(src, dest)
 
         # adjust for p.constants.ratio
         self.sst1._input *= (1-p.constants.ratio)
         self.sst2._input *= (1-p.constants.ratio)
+        logging.debug(
+            f'[{t:.4f}] input:   {arr2str([getattr(self,x)._input for x in self.Nodes])}')
         return
 
     @staticmethod
-    def phi(n: NodeState, p: ConstantsParam) -> np.float64:
+    def phi_other(n: NodeState, p: ConstantsParam) -> np.float64:
         v = (p.c_1 * n._input - p.c_0) / p.g_I + p.r_0
         # to keep it as a numpy float
         if v < 0:
             v *= 0
         return v
 
+    @staticmethod
+    def phi_exc(n: NodeState, p: ConstantsParam) -> np.float64:
+        num = p.a*n._input - p.b
+        den = 1 - np.exp(-p.d * num)
+        return num/den
+
     def calc_phis(self, t: np.float64, p: ParameterSet):
         for dest in self.Nodes:
             n_dest: NodeState = getattr(self, dest)
-            n_dest._phi = self.phi(n_dest, p.constants)
+            if dest in ['exc1', 'exc2']:
+                n_dest._phi = self.phi_exc(n_dest, p.constants)
+            else:
+                n_dest._phi = self.phi_other(n_dest, p.constants)
+        logging.debug(
+            f'[{t:.4f}] phi:     {arr2str([getattr(self,x)._phi for x in self.Nodes])}')
+        return
 
     def calc_ds(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
         for node_name in self.Nodes:
@@ -73,6 +88,10 @@ class ModelBase(State):
                 gamma_r_ampa = p.gamma_ampa * n.r
                 d.s_ampa = s_tau_ampa + gamma_r_ampa
 
+        logging.debug(
+            f'[{t:.4f}] ds:      {arr2str([getattr(delta,x).s for x in self.Nodes])}')
+        logging.debug(
+            f'[{t:.4f}] ds_ampa: {arr2str([getattr(delta,x).s_ampa for x in self.Nodes])}')
         return
 
     def calc_dr(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
@@ -85,6 +104,8 @@ class ModelBase(State):
             # d.r = (n._phi - n.r)/params.constants.tau_r + p.gamma_r
             Warning("gamma_r is not a parameter... using gamma insteads")
             d.r = (n._phi - n.r)/params.constants.tau_r + p.gamma
+        logging.debug(
+            f'[{t:.4f}] dr:      {arr2str([getattr(delta,x).r for x in self.Nodes])}')
         return
 
     def calc_dy(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
@@ -92,9 +113,12 @@ class ModelBase(State):
             n: NodeState = getattr(self, node_name)
             d: NodeState = getattr(delta, node_name)
             d.y = n.y / params.constants.tau_n
+        logging.debug(
+            f'[{t:3.4f}] dy:      {arr2str([getattr(delta,x).y for x in self.Nodes])}')
         return
 
     def calcDelta(self, t: np.float64, params: ParameterSet):
+        logging.debug(f'[{t:.4f}] ')
         self.calc_inputs(t, params)
         self.calc_phis(t, params)
 
