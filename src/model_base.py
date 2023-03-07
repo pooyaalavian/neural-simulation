add logs, fix phi overflow/underflow

src/model_base.py
@@ -23,6 +23,13 @@ class ModelBase(State):
         self.vip1.s = 0.05
         self.vip2.s = 0.05
 
+    def log_state(self, t: np.float64, p: ParameterSet):
+        logging.debug(f'  s:       {arr2str([getattr(self,x).s for x in self.Nodes])}')
+        logging.debug(f'  s_ampa:  {arr2str([getattr(self,x).s_ampa for x in self.Nodes])}')
+        logging.debug(f'  r:       {arr2str([getattr(self,x).r for x in self.Nodes])}')
+        logging.debug(f'  y:       {arr2str([getattr(self,x).y for x in self.Nodes])}')
+        return
+
     def calc_inputs(self, t: np.float64, p: ParameterSet):
         for dest in self.Nodes:
             n_dest: NodeState = getattr(self, dest)
@@ -43,12 +50,12 @@ class ModelBase(State):
         self.sst1._input *= (1-p.constants.ratio)
         self.sst2._input *= (1-p.constants.ratio)
         logging.debug(
-            f'[{t:.4f}] input:   {arr2str([getattr(self,x)._input for x in self.Nodes])}')
+            f'  input:   {arr2str([getattr(self,x)._input for x in self.Nodes])}')
         return
 
     @staticmethod
     def phi_other(n: NodeState, p: ConstantsParam) -> np.float64:
-        v = (p.c_1 * n._input - p.c_0) / p.g_I + p.r_0
+        v = ((p.c_1 * n._input - p.c_0) / p.g_I) + p.r_0
         # to keep it as a numpy float
         if v < 0:
             v *= 0
@@ -57,8 +64,24 @@ class ModelBase(State):
     @staticmethod
     def phi_exc(n: NodeState, p: ConstantsParam) -> np.float64:
         num = p.a*n._input - p.b
-        den = 1 - np.exp(-p.d * num)
-        return num/den
+        try:
+            den = 1 - np.exp(-p.d * num)
+            return num/den
+        except FloatingPointError as e:
+            val = np.abs(p.d * num)
+            sgn = np.sign(-p.d * num) 
+            if val > 1e10:
+                Warning('den overflow in calculating phi_exc')
+                if sgn>0:
+                    return 0
+                if sgn<0:
+                    return num 
+                return num
+            if val < 1e-10:
+                Warning('den underflow in calculating phi_exc')
+                # Use Hospital for disambiguation
+                return 1.0/p.d
+            raise FloatingPointError(f'Could not handle...\n{e}')
 
     def calc_phis(self, t: np.float64, p: ParameterSet):
         for dest in self.Nodes:
@@ -68,7 +91,7 @@ class ModelBase(State):
             else:
                 n_dest._phi = self.phi_other(n_dest, p.constants)
         logging.debug(
-            f'[{t:.4f}] phi:     {arr2str([getattr(self,x)._phi for x in self.Nodes])}')
+            f'  phi:     {arr2str([getattr(self,x)._phi for x in self.Nodes])}')
         return
 
     def calc_ds(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
@@ -89,9 +112,9 @@ class ModelBase(State):
                 d.s_ampa = s_tau_ampa + gamma_r_ampa
 
         logging.debug(
-            f'[{t:.4f}] ds:      {arr2str([getattr(delta,x).s for x in self.Nodes])}')
+            f'  ds:      {arr2str([getattr(delta,x).s for x in self.Nodes])}')
         logging.debug(
-            f'[{t:.4f}] ds_ampa: {arr2str([getattr(delta,x).s_ampa for x in self.Nodes])}')
+            f'  ds_ampa: {arr2str([getattr(delta,x).s_ampa for x in self.Nodes])}')
         return
 
     def calc_dr(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
@@ -102,10 +125,12 @@ class ModelBase(State):
             # a new variable gamma_r is referenced here
             # TODO: add to parameterSet.NodeParams?
             # d.r = (n._phi - n.r)/params.constants.tau_r + p.gamma_r
-            Warning("gamma_r is not a parameter... using gamma insteads")
-            d.r = (n._phi - n.r)/params.constants.tau_r + p.gamma
+            # Warning("gamma_r is not a parameter... using gamma insteads")
+            # d.r = (n._phi - n.r)/params.constants.tau_r + p.gamma
+
+            d.r = (n._phi - n.r)/params.constants.tau_r * p.gamma_r
         logging.debug(
-            f'[{t:.4f}] dr:      {arr2str([getattr(delta,x).r for x in self.Nodes])}')
+            f'  dr:      {arr2str([getattr(delta,x).r for x in self.Nodes])}')
         return
 
     def calc_dy(self, delta: 'ModelBase', t: np.float64, params: ParameterSet):
@@ -114,11 +139,12 @@ class ModelBase(State):
             d: NodeState = getattr(delta, node_name)
             d.y = n.y / params.constants.tau_n
         logging.debug(
-            f'[{t:3.4f}] dy:      {arr2str([getattr(delta,x).y for x in self.Nodes])}')
+            f'  dy:      {arr2str([getattr(delta,x).y for x in self.Nodes])}')
         return
 
     def calcDelta(self, t: np.float64, params: ParameterSet):
-        logging.debug(f'[{t:.4f}] ')
+        logging.debug(f'[time {t:.4f}] ')
+        self.log_state(t, params)
         self.calc_inputs(t, params)
         self.calc_phis(t, params)
 